a26a735e0eab16c795164f10e75257ae366741ed
Your Name <you@example.com>
2025-08-27 08:11:08 +0300

    fixed errors

--- movie-app/src/app/layout.tsx
+++ movie-app/src/app/layout.tsx
@@ -1,17 +1,15 @@
+import './globals.css';
+import { Nunito } from 'next/font/google';
 
-import "./globals.css";
-import {Nunito} from "next/font/google";
-
-const nunito = Nunito ({
+const nunito = Nunito({
   subsets: ['latin'],
   weight: ['300', '600', '800'],
-})
-
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, backgroundColor: "#181818", color: "white", fontFamily: "Nunito, sans-serif" }}>
+      <body className={nunito.className} style={{ margin: 0, backgroundColor: '#181818', color: 'white' }}>
         {children}
       </body>
     </html>
