2e6282f18b21ec69355b6aed9122399fd990cd0d
Your Name <you@example.com>
2025-08-27 07:36:27 +0300

    added changes

--- movie-app/src/app/page.tsx
+++ movie-app/src/app/page.tsx
@@ -23,10 +23,8 @@ export default function Home() {
       <header className="flex justify-between items-center p-5">
         <h1 className="text-3xl font-extrabold">Moovie</h1>
         <SearchBar />
-        <Link href="/signin"><button className="ml-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
-          Signin
-        </button></Link>
-        <Link href="/favourites">
+        <Link href="/login"><button className="ml-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Signin</button></Link>
+        <Link href="/favorites">
           <button className="ml-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Go to Favorites</button>
         </Link>
       </header>
